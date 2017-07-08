1c6daf4a4b6c900df1c43f87bd8d4ec9a3a69d99
arvego <stainofmind77@gmail.com>
2017-07-08 19:13:28 +0000

    Python script with required data for mm-randbot.py (such as: BotFather token, path to DATA folder and list of keks).

---
+++ data.py
@@ -0,0 +1,51 @@
+#_*_ coding: utf-8 _*_
+
+myToken = "<YOUR BOTFATHER TOKEN GOES HERE>"
+owmToken = "<YOUR OPENWEATHERMAPS TOKEN GOES HERE>"
+myChatID = <YOUR TELEGRAM CHAT ID GOES HERE>
+myDir = "<FULL PATH WITH YOUR DATA FOLDER GOES HERE>"
+myListOfSubjects={"algebra", "calculus", "funcan", "challenge", "anime"}
+myListOfOther={"fact", "roulette", "memes", "kek"}
+myListOfHate4Kek=[
+	"Fuck your kek.",
+	"Shove yer kek up yer arse.",
+	"KEKBLET.",
+	"Лолкек",
+	"лоличи-кекичи",
+	"Кекайте, дети мои! Кекайте, пока интернетики не забанили.",
+	"FUCK SALT",
+	"Temporary unavailible. Please try again later."
+	"Товарищи, плиз, фак офф.",
+	"Что я наделал... простите меня!",
+	"kekosov",
+	"\"Это вас просто вовремя не отчислили.\"",
+	"Вам бы всё кеканьки да каканьки...",
+	"Agent Smith is the hero mehmat deserves, but not the one it needs... anymore.",
+	"Отстань, а.",
+	"Ты можешь кекнуть, но знай -- я могу кикнуть.",
+	"Чекни /weather.",
+	"I am Error.",
+	"XOB VLR COLJ QEB JXQOFU?",
+	"BSBOVQEFKD FP CFKFQB. BSBK VLR.",
+	"1FED41E48F8E78A46820074CE34E05D0F93D42D3",
+	"CE2D15EBFBBEDE99BE34F805DBA03ED5B6EB7F7A",
+	"Ну что кекаете? Что сразу кекаете-то? Сидят в своих интернетиках и кекают... К сессии-то готовы? Домашку на завтра сделали? Курсач начали техать?... Носки-то хоть постирали? Нет. Кекают тут. Что за народ...",
+	"Не, двач уже не торт.",
+	"Дахорош.",
+	"Косов шлёт сигналы SOSов~!",
+	"\"Косов поел абрикосов,\nКосов поел кокосов,\nКосов поел насосов,\nНо инфа -- сотка:\nКосов не ел\nПисосов.\"\n\n Любовное стихотворение Глеба.",
+	"@rm_bk, забань этих чертей!",
+	"Y3gnS>LV/)MNzB\n\n28 53 86 90 2 12 13 9"
+
+]
+
+wikiLangs=["en", "ru"]
+myListOfURLs_Algebra={}
+myListOfURLs_Calculus={}
+myListOfURLs_FuncAn={}
+
+guessNum_Attempts=5 #(на самом деле их +1, я говнокодер)
+							#кажется, исправил. Проще было бы заново код написать...
+guessNum_maxNum=30
+
+myKillswitch="<YOUR CODE WORD FOR KILLING THE BOT FROM A CHAT GOES HERE>"
