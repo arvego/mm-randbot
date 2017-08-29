7f5c3a7c5377910feebf54f8643ed1be0ba80f90
Artemy Gevorkov <stainofmind77@gmail.com>
2017-08-29 21:26:21 +0400

    Update bot_status.sh

--- bot_status.sh
+++ bot_status.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
+STATUS=0                            `#переменная для бесконечного цикла`
 FILENAME="iamdown.txt"              `#переменная для упавшего бота (из data.py)`
 FILENAME_KILL="theykilledme.txt"    `#переменная для убитого бота (из data.py)`
 NOTIFIED=0                          `#переменные чтобы не флудить консоль`
 NOTIFIED_KILL=0
 
 `#инициализация вечного цикла`
-while true; do
+while [ $STATUS -eq "0" ]; do
 `#если бот лежит и не убит нами, или если не запущен вовсе, то воскрешаем его`
-    if [ -e $FILENAME ] && [ ! -e $FILENAME_KILL ] || [ $(ps -ef | grep "python ./mm-randbot.py" | grep -v "grep" | wc -l) -eq "0" ]; then
+    if [ -e $FILENAME ] && [ ! -e $FILENAME_KILL ] || [ $(ps -ef | grep "python ./mm-randbot.py" | grep -v "grep" | wc -l) -eq "0" ] && [ ! -e $FILENAME_KILL ]; then
         printf "Bot is down. Ressurecting it.\n-----------------------------\n\n"
         NOTIFIED=0
         NOTIFIED_KILL=0
