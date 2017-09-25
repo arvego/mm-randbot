08a2c07f360017fae1116da65f6f167ca87194f7
uburuntu <bekbulatov.ramzan@ya.ru>
2017-09-25 13:35:44 +0300

    fix: kill in update

--- bot_update.sh
+++ bot_update.sh
@@ -4,8 +4,8 @@ FILENAME="runningupdate.txt"
 
 if [ -e $FILENAME ]; then
     # Stop all bot processes
-    kill $(ps aux | grep '[p]ython2 mm-randbot.py' | awk '{print $2}')
-    kill $(ps aux | grep '[b]ash bot_status.sh' | awk '{print $2}')
+    kill -9 $(ps aux | grep 'mm-randbot.py' | awk '{print $2}')
+    kill -9 $(ps aux | grep 'bot_status.sh' | awk '{print $2}')
     # Stage all changes
     git add -A
     git commit -m "bot_update: unpushed changes"
