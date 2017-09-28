0bb9e70ec0d6f03083953f945d617380ebe617d7
Artemy Gevorkov <stainofmind77@gmail.com>
2017-09-29 03:44:00 +0400

    hotfix: getting updated version

--- bot_update.sh
+++ bot_update.sh
@@ -6,12 +6,10 @@ if [ -e $FILENAME ]; then
     # Stop all bot processes
     kill -9 $(ps aux | grep 'mm-randbot.py' | awk '{print $2}')
     kill -9 $(ps aux | grep 'bot_status.sh' | awk '{print $2}')
-    # Stage all changes
-    git add -A
-    git commit -m "bot_update: unpushed changes"
-    # Update repo
-    git pull --rebase
-    rm $FILENAME
-    # Run bot
+
+    cd ..
+    rm -rf mm-randbot/
+    git clone -b "disa_branch" https://github.com/arvego/mm-randbot.git
+    cd mm-randbot/
     bash bot_status.sh
 fi
