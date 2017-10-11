abbc83af9fcecf3429d1afef3399b7a482708520
uburuntu <bekbulatov.ramzan@ya.ru>
2017-10-11 16:53:14 +0300

    Enhance /wf: add name to doc, fix log and output

--- commands/wolfram.py
+++ commands/wolfram.py
@@ -29,31 +29,24 @@ def wolframSolver(message):
     if not len(message.text.split()) == 1:
         my_bot.send_chat_action(message.chat.id, 'upload_photo')
         your_query = ' '.join(message.text.split()[1:])
-        user_action_log(message,
-                        "entered this query for /wolfram:\n"
-                        "{0}".format(your_query))
-        response = requests.get("https://api.wolframalpha.com/v1/simple?appid="
-                                + tokens.wolfram,
+        user_action_log(message, "entered this query for /wolfram:\n{0}".format(your_query))
+        response = requests.get("https://api.wolframalpha.com/v1/simple?appid=" + tokens.wolfram,
                                 params={'i': your_query})
         # если всё хорошо, и запрос найден
         if response.status_code == 200:
             img_original = Image.open(io.BytesIO(response.content))
-            img_cropped = img_original.crop((0, 95, 540,
-                                             img_original.size[1] - 50))
-            print("{}  {}".format(img_cropped.size[0], img_cropped.size[1]))
-            temp = io.BytesIO()
-            img_cropped.save(temp, format="png")
-            temp.seek(0)
+            img_cropped = img_original.crop((0, 95, 540, img_original.size[1] - 50))
+            io_img = io.BytesIO()
+            io_img.name = "wolfram {}.png".format(your_query.replace("/", "_"))
+            img_cropped.save(io_img, format="png")
+            io_img.seek(0)
             if img_cropped.size[1] / img_cropped.size[0] > data.constants.wolfram_max_ratio:
-                print("Big image here.")
-                my_bot.send_document(message.chat.id, temp,
+                my_bot.send_document(message.chat.id, io_img,
                                      reply_to_message_id=message.message_id)
             else:
-                my_bot.send_photo(message.chat.id, temp,
+                my_bot.send_photo(message.chat.id, io_img,
                                   reply_to_message_id=message.message_id)
-                user_action_log(message,
-                                "has received this Wolfram output:\n"
-                                "{0}".format(response.url))
+            user_action_log(message, "has received this Wolfram output:\n{0}".format(response.url))
         # если всё плохо
         else:
             my_bot.reply_to(message,
@@ -64,9 +57,7 @@ def wolframSolver(message):
                                 data.constants.time,
                                 time.gmtime()),
                                 message.from_user.id))
-            # если пользователь вызвал /wolfram без аргумента
+    # если пользователь вызвал /wolfram без аргумента
     else:
-        my_bot.reply_to(message,
-                        "Я не понял запрос.\nДля вызова Wolfram вводи команду "
-                        "в виде `/wolfram <запрос>` или `/wf <запрос>`.")
+        my_bot.reply_to(message, "Использование: `/wolfram <запрос>` или `/wf <запрос>`", parse_mode="Markdown")
         user_action_log(message, "called /wolfram without any arguments")
