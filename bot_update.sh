a072c8c2a24477b871a6fcc00ccaca2222833fd1
number Zero <silverunicorn2011@yandex.ru>
2017-11-02 21:35:55 +0300

    Forced update

--- bot_update.sh
+++ bot_update.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 echo "=== UPDATING THE BOT ==="
+git reset --hard
 git pull < /dev/null &&
 echo "=== UPDATE COMPLETE ===" &&
 exec ./mm-randbot.py
 
+# if anything went wrong...
 echo "!!! UPDATE FAILED !!!"
+git reset --hard
