b526a2ef715b3ed4e3ce0bf36fed5230c8386496
arvego <stainofmind77@gmail.com>
2017-11-25 17:18:18 +0300

    add: Facebook forwarding, Vspominashki insta-ban; fix:/me, requirements.txt

--- vk/vk_utils.py
+++ vk/vk_utils.py
@@ -39,6 +39,7 @@ class VkPost:
         self.audio_links = []
         self.video_links = []
         self.footer_text_fb = ''
+        self.links_fb = []
         self.gif_links_fb = []
         self.image_links_fb = []
         self.audio_links_fb = []
@@ -59,9 +60,6 @@ class VkPost:
         self.final_text = post_text
 
     def prepare_post_fb(self):
-        # Предварительная обработка
-        self.attachments_handle_fb()
-
         # Подготовка текстовой части
         self.init_header()
         self.body_text = self.post['text'] if not self.is_repost else self.post['copy_history'][-1]['text']
@@ -89,7 +87,7 @@ class VkPost:
 
     def send_post_fb(self):
         api = facebook.GraphAPI(tokens.fb)
-        '''
+
         if len(self.image_links) > 1:
             response = requests.get(self.image_links[0])
             pic = Image.open(io.BytesIO(response.content))
@@ -111,6 +109,11 @@ class VkPost:
             pic.save(pic_byte, format="png")
             pic_byte.seek(0)
             status = api.put_photo(image=pic_byte, message=self.final_text_fb)
+        if len(self.links_fb) > 0:
+            status = api.put_object(
+                        parent_object="me", connection_name="feed",
+                        message=self.final_text_fb,
+                        link=self.links_fb[0])
         elif len(self.gif_links) > 0 or len(self.audio_links) > 0 or len(self.video_links) > 0:
             my_media = first((self.gif_links, self.audio_links, self.video_links), key=lambda x: len(x)>0)
             status = api.put_object(
@@ -120,13 +123,16 @@ class VkPost:
         else:
             status = api.put_object(
                         parent_object="me", connection_name="feed",
-                        message=self.final_text_fb)
-        '''
+                        message=self.final_text_fb,
+                        link="https://vk.com/wall{}_{}".format(self.owner_id, self.post['id']))
+        # вариант Морозова
         my_link = "https://vk.com/wall{}_{}".format(self.owner_id, self.post['id'])
+        '''
         status = api.put_object(
-                    parent_object="me", connection_name="feed",
-                    message="",
-                    link=my_link)
+                 parent_object="me", connection_name="feed",
+                 message="",
+                 link=my_link)
+        '''
 
 
     def not_posted(self):
@@ -195,6 +201,14 @@ class VkPost:
         text_album = ''
         text_audio = ''
         text_video = ''
+        text_link_fb = ''
+        text_docs_fb = ''
+        text_note_fb = ''
+        text_poll_fb = ''
+        text_page_fb = ''
+        text_album_fb = ''
+        text_audio_fb = ''
+        text_video_fb = ''
 
         def log_extraction(attach_type, url='no url'):
             print("  Successfully extracted {} URL: {}\n".format(attach_type, url))
@@ -221,11 +235,13 @@ class VkPost:
                     log_extraction(attachment['type'], attach_url)
                 elif 'platform' in attachment['video']:
                     text_video += "\n— Видео:\n{}\n".format(attachment['video']['title'])
+                    text_video_fb += "\n— Видео:\n{}\n".format(attachment['video']['title'])
                 else:
                     attach_owner = attachment['video']['owner_id']
                     attach_id = attachment['video']['id']
                     attach_url = "https://vk.com/video{}_{}".format(attach_owner, attach_id)
                     text_video += "\n— Видео:\n<a href=\"{}\">{}</a>\n".format(attach_url, attachment['video']['title'])
+                    text_video_fb += "\n— Видео:\n{}\n".format(attachment['video']['title'])
                     # self.video_links.append(attach_url) # Временно отключено до разбирательств (@rm_bk)
                     log_extraction(attachment['type'], attach_url)
 
@@ -233,6 +249,8 @@ class VkPost:
                 attach_url = attachment['audio']['url']
                 if attach_url.endswith("audio_api_unavailable.mp3"):
                     text_audio += "\n— Аудио:\n{} — {}.\n".format(attachment['audio']['artist'],
+                                                                  attachment['audio']['title'])
+                    text_audio_fb += "\n— Аудио:\n{} — {}.\n".format(attachment['audio']['artist'],
                                                                   attachment['audio']['title'])
                 else:
                     self.audio_links.append(attach_url)
@@ -245,15 +263,20 @@ class VkPost:
                 else:
                     if first_doc:
                         text_docs += "\n— Приложения:\n"
+                        text_docs_fb += "\n— Приложения:\n"
                         first_doc = False
                     text_docs += "<a href=\"{}\">{}</a>" \
                                  ", {} Мб\n".format(attach_url, attachment['doc']['title'],
+                                                    round(attachment['doc']['size'] / 1024 / 1024, 2))
+                    text_docs_fb += "{1}:\n{0}  ({2} Мб)\n".format(attach_url, attachment['doc']['title'],
                                                     round(attachment['doc']['size'] / 1024 / 1024, 2))
                 log_extraction(attachment['type'], attach_url)
 
             if attachment['type'] == 'link':
                 attach_url = attachment['link']['url']
                 text_link += "\n— Ссылка:\n<a href=\"{}\">{}</a>\n".format(attach_url, attachment['link']['title'])
+                text_link_fb += "\n— Ссылка ({1}):\n{0}\n".format(attach_url, attachment['link']['title'])
+                self.links_fb.append(attach_url)
                 self.web_preview_url = attachment['link']['preview_url'] if 'preview_url' in attachment[
                     'link'] else attach_url
                 log_extraction(attachment['type'], attach_url)
@@ -261,128 +284,37 @@ class VkPost:
             if attachment['type'] == 'note':
                 attach_url = attachment['note']['view_url']
                 text_note += "\n— Заметка:\n<a href=\"{}\">{}</a>\n".format(attach_url, attachment['note']['title'])
+                text_note_fb += "\n— Заметка ({1}):\n{0}\n".format(attach_url, attachment['note']['title'])
                 log_extraction(attachment['type'], attach_url)
 
             if attachment['type'] == 'poll':
                 text_poll += "\n— Опрос:\n«{}», голосов: {}\n".format(attachment['poll']['question'],
                                                                       attachment['poll']['votes'])
+                text_poll_fb += "\n— Опрос:\n«{}», голосов: {}\n".format(attachment['poll']['question'],
+                                                                  attachment['poll']['votes'])
                 for answer in attachment['poll']['answers']:
                     text_poll += "  → {}, голосов: {}\n".format(answer['text'], answer['votes'])
+                    text_poll_fb += "  → {}, голосов: {}\n".format(answer['text'], answer['votes'])
+
                 log_extraction(attachment['type'])
 
             if attachment['type'] == 'page':
                 attach_url = attachment['page']['view_url']
                 text_page += "\n— Вики-страница:\n<a href=\"{}\">{}</a>\n".format(attach_url,
                                                                                   attachment['page']['title'])
+                text_page += "\n— Вики-страница ({1}):\n{0}\n".format(attach_url,
+                                                                                  attachment['page']['title'])
                 log_extraction(attachment['type'], attach_url)
 
             if attachment['type'] == 'album':
                 text_album += "\n— Альбом:\n{}, {} фото\n".format(attachment['album']['title'],
+                                                                  attachment['album']['size'])
+                text_album_fb += "\n— Альбом:\n{}, {} фото\n".format(attachment['album']['title'],
                                                                   attachment['album']['size'])
                 log_extraction(attachment['type'])
 
         self.footer_text = text_poll + text_link + text_docs + text_video + text_audio + text_note + text_page + text_album
-
-
-    def attachments_handle_fb(self):
-        first_doc = True
-        text_link = ''
-        text_docs = ''
-        text_note = ''
-        text_poll = ''
-        text_page = ''
-        text_album = ''
-        text_audio = ''
-        text_video = ''
-
-        def log_extraction(attach_type, url='no url'):
-            print("  Successfully extracted {} URL for Facebook: {}\n".format(attach_type, url))
-
-        for attachment in self.attachments:
-            '''
-            if attachment['type'] == 'photo':
-                for size in ['photo_1280', 'photo_807', 'photo_604', 'photo_130', 'photo_75']:
-                    if size in attachment['photo']:
-                        attach_url = attachment['photo'][size]
-                        self.image_links.append(attach_url)
-                        log_extraction(attachment['type'], attach_url)
-                        break
-            '''
-
-            if attachment['type'] in ['posted_photo', 'graffiti', 'app']:
-                attach_url = attachment[attachment['type']]['photo_604']
-                self.image_links_fb.append(attach_url)
-                log_extraction(attachment['type'], attach_url)
-
-            if attachment['type'] == 'video':
-                if 'player' in attachment['video']:
-                    attach_url = attachment['video']['player']
-                    # self.video_links.append(attach_url)
-                    self.web_preview_url = attach_url
-                    log_extraction(attachment['type'], attach_url)
-                elif 'platform' in attachment['video']:
-                    text_video += "\n— Видео:\n{}\n".format(attachment['video']['title'])
-                else:
-                    attach_owner = attachment['video']['owner_id']
-                    attach_id = attachment['video']['id']
-                    attach_url = "https://vk.com/video{}_{}".format(attach_owner, attach_id)
-                    text_video += "\n— Видео ({1}):\n{0}\n".format(attach_url, attachment['video']['title'])
-                    # self.video_links.append(attach_url) # Временно отключено до разбирательств (@rm_bk)
-                    log_extraction(attachment['type'], attach_url)
-
-            if attachment['type'] == 'audio':
-                attach_url = attachment['audio']['url']
-                if attach_url.endswith("audio_api_unavailable.mp3"):
-                    text_audio += "\n— Аудио:\n{} — {}.\n".format(attachment['audio']['artist'],
-                                                                  attachment['audio']['title'])
-                else:
-                    self.audio_links_fb.append(attach_url)
-                    log_extraction(attachment['type'], attach_url)
-
-            if attachment['type'] == 'doc':
-                attach_url = attachment['doc']['url']
-                if attachment['doc']['ext'] == 'gif':
-                    self.gif_links_fb.append(attach_url)
-                else:
-                    if first_doc:
-                        text_docs += "\n— Приложения:\n"
-                        first_doc = False
-                    text_docs += "{1}:\n{0}  ({2} Мб)\n".format(attach_url, attachment['doc']['title'],
-                                                    round(attachment['doc']['size'] / 1024 / 1024, 2))
-                log_extraction(attachment['type'], attach_url)
-
-            if attachment['type'] == 'link':
-                attach_url = attachment['link']['url']
-                text_link += "\n— Ссылка ({1}):\n{0}\n".format(attach_url, attachment['link']['title'])
-                self.web_preview_url = attachment['link']['preview_url'] if 'preview_url' in attachment[
-                    'link'] else attach_url
-                log_extraction(attachment['type'], attach_url)
-
-            if attachment['type'] == 'note':
-                attach_url = attachment['note']['view_url']
-                text_note += "\n— Заметка ({1}):\n{0}\n".format(attach_url, attachment['note']['title'])
-                log_extraction(attachment['type'], attach_url)
-
-            if attachment['type'] == 'poll':
-                text_poll += "\n— Опрос:\n«{}», голосов: {}\n".format(attachment['poll']['question'],
-                                                                      attachment['poll']['votes'])
-                for answer in attachment['poll']['answers']:
-                    text_poll += "  → {}, голосов: {}\n".format(answer['text'], answer['votes'])
-                log_extraction(attachment['type'])
-
-            if attachment['type'] == 'page':
-                attach_url = attachment['page']['view_url']
-                text_page += "\n— Вики-страница:\n<a href=\"{}\">{}</a>\n".format(attach_url,
-                                                                                  attachment['page']['title'])
-                log_extraction(attachment['type'], attach_url)
-
-            if attachment['type'] == 'album':
-                text_album += "\n— Альбом:\n{}, {} фото\n".format(attachment['album']['title'],
-                                                                  attachment['album']['size'])
-                log_extraction(attachment['type'])
-
-        self.footer_text_fb = text_poll + text_link + text_docs + text_video + text_audio + text_note + text_page + text_album
-
+        self.footer_text_fb = text_poll_fb + text_link_fb + text_docs_fb + text_video_fb + text_audio_fb + text_note_fb + text_page_fb + text_album_fb
 
 
 def replace_wiki_links(text):
