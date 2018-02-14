5ed15f48cbb20ac48c631e3db1ba30f59fd3bd66
uburuntu <bekbulatov.ramzan@ya.ru>
2018-02-15 01:06:43 +0300

    test: some utils

--- tests/bot_tests.py
+++ tests/bot_tests.py
@@ -1,5 +1,18 @@
 #!/usr/bin/env python
 # _*_ coding: utf-8 _*_
 
+import os
+import telebot
+import pytest
+
+token = os.getenv('TG_TOKEN')
+chat_id = os.getenv('TG_CHATID')
+
+if token is None or chat_id is None:
+    pytest.fail(msg="Environment vars TG_TOKEN and TG_CHATID should be set")
+
+my_bot = telebot.TeleBot(token, threaded=True)
+
+
 def test_example():
-    assert True == True
+    assert True is True
